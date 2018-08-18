bc969a2b38019eb55a4f08adfaa1a115768f717d
conqsolid <conqsolid@gmail.com>
2018-08-18 15:25:21 +0300

    oauth2

---
+++ src/ConqSolid.Web/Constants/AuthorizationConstants.cs
@@ -0,0 +1,8 @@
+namespace ConqSolid.Web.Constants
+{
+    public struct AuthorizationConstants
+    {
+        public const string CLIENT_ID = "935599603417-ao1hm8qnaifei6njpiu32n7au9r7gbpb.apps.googleusercontent.com";
+        public const string CLIENT_SECRET = "B6m_hvIhNpvgQkDoCaXkcxMi"; 
+    }
+}
